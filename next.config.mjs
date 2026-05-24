3759395c486e3de4272dbf42cfd45466e2a0c717
PeachCoffee <hknu123dmddo@gmail.com>
2026-05-24 20:11:17 +0900

    fix pwa manifest and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,15 @@
-import withPWA from 'next-pwa';
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
-export default withPWA({
-  dest: 'public',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'ai-images',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        },
-      },
-    },
-  ],
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
+
+export default withPWA(nextConfig);
