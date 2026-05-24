7e4a4fce01ce557a86a5c0a44dcdfee62a744422
PeachCoffee <hknu123dmddo@gmail.com>
2026-05-24 19:11:58 +0900

    fix pwa production

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default withPWA(nextConfig);
